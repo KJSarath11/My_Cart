cccec538c6bbc6693fb4794bde6d91edaf3f8e0c
Sarath K J <kjsarath76@gmail.com>
2024-12-24 17:09:28 +0530

    Product list added

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index)
+    path('',views.index,name="home"),
+    path('product_list',views.list_products,name="list_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 #! Product View
 def index(request):
     return render(request,"index.html")
+
+def list_products(request):
+    return render(request,"products.html")
+
+def detail_product(request):
+    return render(request,"product_detail.html")
