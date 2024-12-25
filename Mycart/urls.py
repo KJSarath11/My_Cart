bd7e6ddd83deaf58606882d1e3042c03395dbff9
Sarath K J <kjsarath76@gmail.com>
2024-12-25 10:09:50 +0530

    Order page done and all layouts completed

--- Mycart/urls.py
+++ Mycart/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('products.urls')),
     path("customer/",include('customers.urls')),
+    path("orders/",include('orders.urls')),
 ]
 
 if settings.DEBUG:
